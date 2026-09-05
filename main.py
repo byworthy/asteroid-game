ff94845d5446221dc1290e7ed114b76359797a65
byworthy <worthyelevates@gmail.com>
2026-09-05 18:04:45 +0100

    Adding movement and rotation logic

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -20,7 +26,12 @@ def main():
 
         screen.fill("black")
 
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
